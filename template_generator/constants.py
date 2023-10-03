278679d44147ff96a7c0a0f09d6714481ae896aa
Indrajit <indrajitghosh912@gmail.com>
2023-10-03 12:59:00 +0530

    index.html template text modified

--- template_generator/constants.py
+++ template_generator/constants.py
@@ -168,14 +168,15 @@ Google Fonts API:
 -->
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Home</title>
-    </head>
-
-    <body>
-        <h1>Hello World!</h1>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
 </html>
 """
 
